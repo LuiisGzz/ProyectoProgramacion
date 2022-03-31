2fb25dc2d18a7f3b03d2b702be0876bae002f3bc
LuiisGzz <100230827+LuiisGzz@users.noreply.github.com>
2022-03-31 15:09:14 -0600

    Update ProyectoProgramacion.cpp

--- ProyectoProgramacion.cpp
+++ ProyectoProgramacion.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <string>
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
-int opcion, i;
-string Paciente, Hora, Tratamiento, Descripcion, PUtratamiento, Cantidad, PrecioUnitario, Total;
-
-void main()
+struct dentistas
 {
+
+	char Paciente[30], Hora[30], Tratamiento[30], Descripcion[30], PUtratamiento[30], Cantidad[30], PrecioUnitario[30], Total[30];
+}citas[4];
+
+int main()
+{
+
+	int i = 0, opcion;
+
 	while (1 == 1) {
 		cout << "Bienvenido" << endl;
 		cout << "Citas Dentales" << endl;
@@ -23,40 +31,58 @@ void main()
 		switch (opcion)
 		{
 
-		case 1:
+		case 1: {
 			i = i + 1;
 
 			cout << "El numero de su cita es: " << i << endl;
 
 			cout << "Ingrese el nombre del paciente" << endl;
-			cin >> Paciente;
+			cin.ignore();
+			fgets(citas[i].Paciente, 30, stdin);
 
 			cout << "Ingrese la hora de la cita" << endl;
-			cin >> Hora;
+			fgets(citas[i].Hora, 30, stdin);
 
 			cout << "Ingrese el Nombre del Tratamiento" << endl;
-			cin >> Tratamiento;
+			fgets(citas[i].Tratamiento, 30, stdin);
 
 			cout << "Ingrese la descripcion" << endl;
-			cin >> Descripcion;
+			fgets(citas[i].Descripcion, 30, stdin);
 
 			cout << "Ingrese el Precio Unitario del Tratamiento" << endl;
-			cin >> PUtratamiento;
+			fgets(citas[i].PUtratamiento, 30, stdin);
 
 			cout << "Ingrese la Cantidad de Tratamiento" << endl;
-			cin >> Cantidad;
+			fgets(citas[i].Cantidad, 30, stdin);
 
 			cout << "Ingrese el Precio Unitario" << endl;
-			cin >> PrecioUnitario;
+			fgets(citas[i].PrecioUnitario, 30, stdin);
 
 			cout << "Ingrese el Total" << endl;
-			cin >> Total;
+			fgets(citas[i].Total, 30, stdin);
 
 			cout << "Cita agendada correctamente" << endl;
 			break;
+		}
 
+		case 4: {
+			for (int i = 1; i < 4; i++)
+			{
+				cout << "Cita numero " << i << endl;
+				cout << "Paciente: " << citas[i].Paciente << endl;
+				cout << "Hora: " << citas[i].Hora << endl;
+				cout << "Tratamiento: " << citas[i].Tratamiento << endl;
+				cout << "Descripcion: " << citas[i].Descripcion << endl;
+				cout << "Precio Unitario: " << citas[i].PUtratamiento << endl;
+				cout << "Cantidad:" << citas[i].Cantidad << endl;
+				cout << "Precio Unitario del Tratamiento: " << citas[i].PrecioUnitario << endl;
+				cout << "Total: " << citas[i].Total << endl;
+
+			}
+			break;
+		}
 		case 6:
-			return;
+			exit(EXIT_SUCCESS);;
 			break;
 		default:
 			cout << "Favor de ingresar una de las opciones" << endl;
@@ -66,4 +92,3 @@ void main()
 
 	}
 }
-//
